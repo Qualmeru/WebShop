fbcb7fefc091823ed2f34d89c3f7a7c8273e06d7
WowbaggerIP <wowbaggerip@hotmail.com>
2014-11-21 15:28:09 +0100

    Person class

--- WebShop/Person.cs
+++ WebShop/Person.cs
@@ -9,8 +9,22 @@ namespace WebShop
 {
     public class Person
     {
+       
+        
         [Key]
         public int Id { get; set; }
         public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+        public string Email {get; set;}
+        public enum Admin
+        {
+            User = 1,
+            Admin = 2
+        }
+        public Admin Admin { get; set; }
+        public virtual ICollection<Order> Order {get; set;}
+
     }
 }
