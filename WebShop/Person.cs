fab3820e318f5e2678e5453d392558f566647684
WowbaggerIP <wowbaggerip@hotmail.com>
2014-11-21 15:16:35 +0100

    testar

--- WebShop/Person.cs
+++ WebShop/Person.cs
@@ -10,6 +10,11 @@ namespace WebShop
     public class Person
     {
         [Key]
-        public int Id { get; set; } 
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
     }
 }
