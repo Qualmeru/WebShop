35c3648e549fc664e00438d931871ab71e27275c
WowbaggerIP <wowbaggerip@hotmail.com>
2014-11-24 11:11:26 +0100

    MVC

--- WebShopMVC/Properties/AssemblyInfo.cs
+++ WebShopMVC/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("58a87766-8e95-4035-b787-a09be9aebefd")]
+[assembly: Guid("319e2aad-0cf1-4661-99d6-c6b742992329")]
 
 // Version information for an assembly consists of the following four values:
 //
