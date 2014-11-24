0f06db04e4bd4d3a54d46169118bbbf14c516e44
Qualmeru <qualmeru@hotmail.com>
2014-11-24 09:14:16 +0100

    MainDB

---
+++ WebShop/Genre.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShop
+{
+    class Genre
+    {
+        public Genre()
+        {
+            Products = new HashSet<Product>();
+        }
+        [Key]
+        public int Id { get; set; }
+        [Column("Genre_name")]
+        public string GenreName { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; } 
+    }
+}
---
+++ WebShop/MainDB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShop
+{
+    class MainDB : DbContext
+    {
+        public MainDB()
+            : base("WebShop")
+        {
+
+        }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Konsol> Konsols { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
